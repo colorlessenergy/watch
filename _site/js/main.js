17324e992ae47b241d1ca993b271edb28dc0c8d5
Daniel Munoz <daniel.munozdev@gmail.com>
2018-05-15 10:53:21 -0700

    add underline animation to links and rename mp3 file

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,8 +1,9 @@
 let eventBox = document.querySelector("body");
 let animate = document.querySelector('.animate');
 
+// follow cursor event listener
 eventBox.addEventListener("tiltChange", function (event) {
   eventBox.style.transform = '';
-  console.log(event);
-  animate.style.transform = 'perspective(500px) rotateX(' +event.detail.tiltY + 'deg) rotateY('+  event.detail.tiltX +'deg) scale3d(1, 1, 1)'
+  // console.log(event);
+  animate.style.transform = 'perspective(500px) rotateX(' +event.detail.tiltY + 'deg) rotateY('+  event.detail.tiltX +'deg) scale3d(1, 1, 1)';
 });
